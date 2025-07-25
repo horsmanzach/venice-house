6fb996e4befa79ddee67de8ed9f68b555c44c329
horsmanzach <zach@magnawebservices.com>
2025-07-24 23:34:01 -0600

    Includes 3 Different Effects Based on MWG Effect

--- js/gsap-effects.js
+++ js/gsap-effects.js
@@ -1,3 +1,22 @@
+//====== Expanding Hero Animation
+
+document.querySelectorAll(".hero_wrap").forEach((heroWrap) => {
+  const heroMask = heroWrap.querySelector(".hero_mask");
+  const heroImg = heroWrap.querySelector(".hero_img");
+  const heroTxt = heroWrap.querySelector(".hero_txt");
+
+  const tl = gsap.timeline({ defaults: { duration: 1, ease: "power2.inOut" } });
+
+  tl.set(heroMask, { opacity: 1 });
+  tl.from(heroMask, { y: "-100vh" });
+  tl.from(heroImg, { y: "-100vh" }, "<");
+  tl.fromTo(
+    heroMask,
+    { clipPath: "inset(calc(50% - 20vw) calc(50% - 20vw) round 1.5rem)" },
+    { clipPath: "inset(calc(0% - 0vw) calc(0% - 0vw) round 1.5rem)" }
+  );
+});
+
 //======Text Effect on Scroll========
 
 gsap.registerPlugin(ScrollTrigger)
@@ -126,3 +145,60 @@ window.addEventListener("DOMContentLoaded", () => {
         rot += angle
     })
 })
+
+
+//====== Full Screen Flipping Cards
+
+gsap.registerPlugin(ScrollTrigger)
+
+window.addEventListener("DOMContentLoaded", () => {
+
+    /* LENIS SMOOTH SCROLL (OPTIONAL) */
+    lenis = new Lenis({
+        autoRaf: true,
+    })
+    /* LENIS SMOOTH SCROLL (OPTIONAL) */
+
+    gsap.to('.scroll', {
+        autoAlpha:0,
+        duration:0.2,
+        scrollTrigger: {
+            trigger:'.mwg_effect031',
+            start:'top top',
+            end:'top top-=1',
+            toggleActions: "play none reverse none"
+        }
+    })
+
+    const slides = document.querySelectorAll('.mwg_effect031 .slide')
+
+    slides.forEach(slide => {
+        const contentWrapper = slide.querySelector('.content-wrapper')
+        const content = slide.querySelector('.content')
+
+        gsap.to(content, {
+            rotationZ: (Math.random() - 0.5) * 10, // RotationZ between -5 and 5 degrees
+            scale: 0.7, // Slight reduction of the content
+            rotationX: 40,
+            ease: 'power1.in', // Starts gradually
+            scrollTrigger: {
+                pin: contentWrapper, // contentWrapper is pinned during the animation
+                trigger: slide, // Listens to the slide’s position
+                start: 'top 0%', // Starts when its top reaches the top of the viewport
+                end: '+=' + window.innerHeight, // Ends 100vh later
+                scrub: true // Progresses with the scroll
+            }
+        })
+
+        gsap.to(content, {
+            autoAlpha: 0, // Ends at opacity: 0 and visibility: hidden
+            ease: 'power1.in', // Starts gradually
+            scrollTrigger: {
+                trigger: content, // Listens to the position of content
+                start: 'top -80%', // Starts when the top exceeds 80% of the viewport
+                end: '+=' + 0.2 * window.innerHeight, // Ends 20% later
+                scrub: true // Progresses with the scroll
+            }
+        })
+    })
+}) 
